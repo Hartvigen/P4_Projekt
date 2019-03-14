f99574d83ef59ae1653cbedbd33d23d38c0464fa
Emil Palmelund Voldby <evoldb17@student.aau.dk>
2019-03-14 14:54:47 +0100

    Attributed grammar for StrucStmts
    WIP: might contain errors

---
+++ P4 Project/P4 Project/AST/Commands/Stmts/IfNode.cs
@@ -0,0 +1,27 @@
+﻿using P4_Project.AST.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_Project.AST.Commands.Stmts
+{
+    class IfNode : StmtNode
+    {
+        ExprNode condition;
+        Block ifstmts;
+        IfNode elsestmt = null;
+
+        public IfNode(ExprNode e, Block b)
+        {
+            condition = e;
+            ifstmts = b;
+        }
+
+        public void SetElse(IfNode i)
+        {
+            elsestmt = i;
+        }
+    }
+}
---
+++ P4 Project/P4 Project/AST/Commands/Stmts/WhileNode.cs
@@ -0,0 +1,21 @@
+﻿using P4_Project.AST.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_Project.AST.Commands.Stmts
+{
+    class WhileNode : StmtNode
+    {
+        ExprNode condition;
+        Block stmts;
+
+        public WhileNode(ExprNode e, Block b)
+        {
+            condition = e;
+            stmts = b;
+        }
+    }
+}
