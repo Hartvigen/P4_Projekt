ae8cad3157182ae67ca6c6bfa03f2c5984504150
sdfg610 <kristianvagner@yahoo.dk>
2019-04-03 13:34:21 +0200

    Auto stash before merge of "Sean" and "origin/Hartvig"

--- P4 Project/P4 Project/Types/Functions/FunctionType.cs
+++ P4 Project/P4 Project/Types/Functions/FunctionType.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace P4_Project.Types.Functions
 {
     public class FunctionType : BaseType
     {
+        public Dictionary<string, BaseType> Parameters { get; private set; }
+        public List<BaseType> TypeList { get => Parameters.Values.ToList(); }
+
+        public FunctionType(Dictionary<string, BaseType> parameters)
+        {
+            Parameters = parameters;
+        }
 
         public override string ToString()
         {
-            return "function";
+            return $"function({string.Join(", ", Parameters.Select(pair => $"{pair.Key} {{{pair.Value.ToString()}}}"))})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 }
