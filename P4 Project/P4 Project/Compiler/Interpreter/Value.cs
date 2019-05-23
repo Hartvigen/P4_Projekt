bfd23611c970a862b953849e4c4ec07755b27911
Jonas Madsen <jmad17@student.aau.dk>
2019-05-23 09:59:59 +0200

    Clean

--- P4 Project/P4 Project/Compiler/Interpreter/Value.cs
+++ P4 Project/P4 Project/Compiler/Interpreter/Value.cs
@@ -37,7 +37,7 @@ namespace P4_Project.Compiler.Interpreter
         private void IdentifyType(object v)
         {
             Type vType = v.GetType();
-            switch (PreDefined.typeMap[vType])
+            switch (PreDefined.TypeMap[vType])
             {
                 case 0: type = new BaseType("none"); break;
                 case 1: type = new BaseType("boolean"); break;
